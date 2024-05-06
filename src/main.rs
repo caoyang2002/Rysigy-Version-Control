9ebe5763df6be88a062ebcdb4e51329fc825b6e4
caoyang2002 <cy12968@163.com>
2024-05-06 23:36:44 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt;
+
+struct Point {
+    x: i32,
+    y: i32,
 }
+
+impl fmt::Debug for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Debug: Point(x: {}, y: {})", self.x, self.y)
+    }
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Display: Point({}, {})", self.x, self.y)
+    }
+}
+
+fn main() {
+    let point = Point { x: 10, y: 20 };
+    println!("a = {:?}, b = {:?}", point, point); // 使用 Debug 格式化
+    println!("a = {}, b = {}", point, point);    // 使用 Display 格式化
+}
+
